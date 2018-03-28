850bf10ab33062c45c901fc3d544f356afc46c06
Zishuo WANG <KELVINWANG82@OUTLOOK.COM>
2018-03-29 01:12:53 +1100

    w5

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path')
 
 app.set('view engine','ejs');
 const router = require('./route/route');
 
 
+app.use( express.static(__dirname + '/views'));
 app.use(router);
 app.listen(PORT, function(){
     console.log(`Express listening on port ${PORT}`);
